6eacd8ffa32eff6bd3939812e12364f3b25ec9ad
huzaifa12max <81131294+huzaifa12max@users.noreply.github.com>
2021-04-19 13:01:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const Discord = require('discord.js');
  const client = new Discord.Client();
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+     res.send("Bot is Online 24/7")
+})
+
+app.listen(3000, () => {
+console.log("Bot is Online 24/7")
+})
 
 client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
